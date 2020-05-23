97564344a1123262cc14259f1ca4533deaa03447
fpdevil <singamsetty.sampath@gmail.com>
2020-05-23 00:51:37 -0500

    split exercise problems...

--- slices/exercises/exercises1/main.go
+++ slices/exercises/exercises1/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	// import the problems package containing exercises
 	"github.com/fpdevil/learngo/slices/exercises/exercises1/problems"
@@ -18,16 +20,36 @@ func main() {
 	problems.Exercise5()
 	problems.Exercise6()
 	problems.Exercise7()
+	problems.Exercise8()
+	problems.Exercise9()
 
-	// var (
-	// 	args = os.Args
-	// 	l    = len(args)
-	// )
+	var (
+		args = os.Args
+		l    = len(args)
+	)
 
-	// // go run main.go ex9 10.5
-	// if l > 2 && args[1] == "ex9" {
-	// 	problems.Exercise9(strings.Join(args[2:], ","))
-	// } else {
-	// 	problems.Exercise9("")
-	// }
+	// go run main.go ex9 4 6 1 3 0 9 2
+	if l > 2 && args[1] == "ex10" {
+		problems.Exercise10(strings.Join(args[2:], " "))
+	} else {
+		problems.Exercise10("")
+	}
+
+	problems.Exercise11()
+	problems.Exercise12()
+	problems.Exercise13()
+
+	// go run main.go ex14 1 2 4
+	if l > 2 && args[1] == "ex14" {
+		problems.Exercise14(strings.Join(args[2:], " "))
+	} else {
+		problems.Exercise14("")
+	}
+
+	// go run main.go ex15 Location
+	if l > 2 && args[1] == "ex15" {
+		problems.Exercise15(strings.Join(args[2:], " "))
+	} else {
+		problems.Exercise15("")
+	}
 }
